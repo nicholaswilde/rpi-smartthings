6e6208f08ed1a2bc46334ba47490038dba6cf633
nicholaswilde <ncwilde43@gmail.com>
2015-03-20 00:02:15 -0700

    Updated postinstall.js

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,11 +1,21 @@
 #!/usr/bin/env node
 
+// File locations
+var deviceFilePath = __dirname + '/../public/device.xml',
+    cpuinfoFilePath = '/proc/cpuinfo'
+
+// Require all npm modules
 var fs = require('fs'),
     xml2js = require('xml2js'),
     parser = new xml2js.Parser({explicitArray : false}),
-    prompt = require('prompt');
+    prompt = require('prompt'),
+    parser2 = require('xmldom').DOMParser,
+    serializer = new (require('xmldom').XMLSerializer);
+
+// Remove Prompt colors
 prompt.colors = false;
 
+// Array for looking up Raspberry Pi model names
 var modelLookup = {
   '0002':       'Model B Revision 1.0',
   '0003':       'Model B Revision 1.0',
@@ -43,7 +53,7 @@ function fullParallel(callbacks, last) {
 
 // Read the Device.xml file for settings
 function readDevice(arg, callback) {
-  fs.readFile(__dirname + '/../public/device.xml', function(err, data) {
+  fs.readFile(deviceFilePath, function(err, data) {
     parser.parseString(data, function (err, result) {
       if (err) { return onErr(err); }
       callback(arg, result.root.device[arg]);
@@ -53,7 +63,7 @@ function readDevice(arg, callback) {
 
 // Read /proc/cpuinfo for settings
 function readCpuinfo(arg, callback) {
-  fs.readFile('/proc/cpuinfo','utf8', function (err, data) {
+  fs.readFile(cpuinfoFilePath,'utf8', function (err, data) {
     if (err)  { return onErr(err); }
     var jsonObj = data.split('\n');
     var result = {}
@@ -69,7 +79,9 @@ function readCpuinfo(arg, callback) {
   });
 }
 
+// Get prompt
 function getPrompt(results) {
+  // Prompt properties and defaults
   var schema = {
     properties: {
       fName: {
@@ -88,7 +100,31 @@ function getPrompt(results) {
     console.log('Command-line input received:');
     console.log('  friendlyName: ' + result.fName);
     console.log('  UDN: ' + result.udn);
+    writeXml(results);
   });
+}
+
+// Write XML Document
+function writeXml(results) {
+  var fileData = fs.readFileSync(deviceFilePath, 'utf8');
+
+  var doc = new parser2().parseFromString(fileData.substring(2, fileData.length));
+
+  var x = doc.documentElement;
+  newNode=doc.createElement("manufacturer");
+  newText=doc.createTextNode("A Notebook");
+
+  newNode.appendChild(newText);
+  var y = doc.getElementsByTagName('manufacturer')[0]
+  x.replaceChild(newNode, y)
+
+  console.log(serializer.serializeToString(x));
+}
+
+// Change node value
+function changeNode(doc, node) {
+  console.log(node);
+  console.log(serializer.serializeToString(doc))
 }
 
 fullParallel([
@@ -98,14 +134,16 @@ fullParallel([
   function(next) { readCpuinfo('Serial', next); }
 ], getPrompt);
 
+// Handle errors
 function onErr(err) {
   console.error(err);
   return -1;
 }
 
+// Find value in multidimensional array
 function findValue(array, nameWeAreLookingFor) {
-    for(var i = 0; i<array.length; i++) {
-        if(array[i][0] === nameWeAreLookingFor) return array[i][1];
-    }
-    return -1;
+  for(var i = 0; i<array.length; i++) {
+      if(array[i][0] === nameWeAreLookingFor) return array[i][1];
+  }
+  return -1;
 }
